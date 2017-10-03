d1b5c01a2d19cd3f4871cc84c7698e036e238788
Walid Alzubaidi <walidalzubaidi@Walids-MacBook-Pro.local>
2017-10-03 14:53:07 +0300

    Add layout

--- Schools/ViewController.swift
+++ Schools/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /* Login Button*/
     @IBAction func loginButton(_ sender: Any) {
         
     }
